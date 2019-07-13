8a1764f876ff503f8bf9da3bfbf49b5bf862e611
Thiago Bianeck <thiagobianeck@gmail.com>
2019-07-13 20:17:41 -0300

    Código Normal

---
+++ src/main/java/BalancoEmpresa.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class BalancoEmpresa {
+
+    private HashMap<String, Divida> dividas = new HashMap<String, Divida>();
+
+    public void resgistraDivida(String credor, String cnpjCredor, double valor) {
+        Divida divida = new Divida();
+        divida.setCredor(credor);
+        divida.setTotal(valor);
+        divida.setCnpjCredor(cnpjCredor);
+
+        dividas.put(cnpjCredor, divida);
+    }
+
+    public void pagaDivida(String cnpjCredor, double valor) {
+        Divida divida = dividas.get(cnpjCredor);
+
+        if(divida != null) {
+            if (valor > 100) {
+                valor -= 8;
+            }
+            divida.setValorPago(divida.getValorPago() + valor);
+        }
+    }
+}
---
+++ src/main/java/Divida.java
@@ -0,0 +1,39 @@
+public class Divida {
+
+    private double total;
+    private double valorPago;
+    private String credor;
+    private String cnpjCredor;
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public double getValorPago() {
+        return valorPago;
+    }
+
+    public void setValorPago(double valorPago) {
+        this.valorPago = valorPago;
+    }
+
+    public String getCredor() {
+        return credor;
+    }
+
+    public void setCredor(String credor) {
+        this.credor = credor;
+    }
+
+    public String getCnpjCredor() {
+        return cnpjCredor;
+    }
+
+    public void setCnpjCredor(String cnpjCredor) {
+        this.cnpjCredor = cnpjCredor;
+    }
+}
